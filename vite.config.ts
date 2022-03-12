6deb23c12c519c5a44879f6c1177dfcdd56c3ce0
daidr <daidr@daidr.me>
2022-03-12 16:56:09 +0800

    feat: sw support

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import VueI18n from '@intlify/vite-plugin-vue-i18n';
 import Components from 'unplugin-vue-components/vite';
 import PurgeIcons from 'vite-plugin-purge-icons';
 import svgLoader from 'vite-svg-loader';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -38,6 +39,45 @@ export default defineConfig({
     // https://github.com/intlify/vite-plugin-vue-i18n
     VueI18n({
       include: [resolve(__dirname, './locales/**')],
+    }),
+    VitePWA({
+      includeAssets: [
+        '/icons/android-chrome-192x192.png',
+        '/icons/android-chrome-512x512.png',
+        '/icons/apple-touch-icon.png',
+        '/icons/apple-touch-icon-precomposed.png',
+        '/icons/browserconfig.xml',
+        '/icons/favicon.ico',
+        '/icons/favicon-16x16.png',
+        '/icons/favicon-32x32.png',
+        '/icons/mstile-70x70.png',
+        '/icons/mstile-144x144.png',
+        '/icons/mstile-150x150.png',
+        '/icons/mstile-310x150.png',
+        '/icons/mstile-310x310.png',
+        '/icons/safari-pinned-tab.svg',
+      ],
+      manifest: {
+        name: 'Truth Table | 真值表',
+        short_name: 'Truth Table',
+        description: '根据逻辑表达式自动生成真值表',
+        icons: [
+          {
+            src: '/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        theme_color: '#ddd6fe',
+        start_url: 'https://tb.daidr.me',
+        background_color: '#f5f3ff',
+        display: 'standalone',
+      },
     }),
   ],
 
